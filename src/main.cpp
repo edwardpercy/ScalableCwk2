049793f4e5ca3e968d76fed89603e77b38bc3b24
fidget spinner <percy.edward@hotmail.co.uk>
2019-11-23 18:22:09 +0000

    Initial Upload

---
+++ src/main.cpp
@@ -0,0 +1,24 @@
+// main.cpp-------------------------------------------------------------------
+/** @file
+* This file starts the program
+*/
+/** Brief description
+* Initiates QT and opens the main windows
+*/
+#include "main.h"
+
+int main( int argc, char** argv )
+{
+		
+	
+
+    //Initiate QT stuff
+
+    // needed to ensure appropriate OpenGL context is created for VTK rendering.
+    QSurfaceFormat::setDefaultFormat( QVTKOpenGLWidget::defaultFormat() );
+    QApplication a( argc, argv );
+    MainWindow window;
+    window.show();
+    return a.exec();
+}
+// /main.cpp------------------------------------------------------------------
